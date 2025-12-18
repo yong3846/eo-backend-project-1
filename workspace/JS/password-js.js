a78eb177ef61d8965d03542eb93aa0105a037a7e
GilYong Hyeon <gusrlfdyd12@gmail.com>
2025-12-18 14:02:34 +0900

    251218_01
    취소버튼 활성화
    패스워드 이름 유효성 검사 추가
    회원가입 완료 후 메인이동

--- workspace/JS/password-js.js
+++ workspace/JS/password-js.js
@@ -1,6 +1,13 @@
 function validateFindPasswordForm() {
+    const name = document.getElementById('name').value;
     const id = document.getElementById('id').value;
     const email = document.getElementById('email').value;
+
+    if (name.trim() === '') {
+        alert('이름을 입력해주세요.');
+        document.getElementById('name');
+        return false;
+    }
 
     if (id.trim() === '') {
         alert('아이디를 입력해주세요.');
@@ -16,5 +23,6 @@ function validateFindPasswordForm() {
     }
 
     alert('비밀번호 찾기 요청을 전송합니다.');
-    return true; 
+    location.href = '../index.html';
+    return false;
 }
